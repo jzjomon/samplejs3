d2ae502da7dfa0e6c0315bef9b92ab5d86b3ef48
jomon jose <jzjomon@gmail.com>
2023-05-14 13:46:27 +0530

    even or odd assignment finished

--- evenorodd.js
+++ evenorodd.js
@@ -1,2 +1,7 @@
 var readlineSync = require("readline-sync");
 let num = readlineSync.question("enter a number");
+if(num%2==0){
+    console.log("you have entered a even number");
+}else{
+    console.log("you have entered a odd number");
+}
